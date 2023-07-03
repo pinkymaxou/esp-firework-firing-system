134df9132aee4f4292a5e36c8c1f77ddbe670672
Max <pinkymaxou08@gmail.com>
2023-07-02 22:53:05 -0400

    MOSFET instead of relay has master switch. Support PWM

--- source/fwi-receiver-fw/main/HardwareGPIO.c
+++ source/fwi-receiver-fw/main/HardwareGPIO.c
@@ -126,13 +126,12 @@ void HARDWAREGPIO_Init()
     #endif
 
     // PWM for master power
-    // Init ramp LED
     // Prepare and then apply the LEDC PWM timer configuration
     ledc_timer_config_t ledc_timer = {
         .speed_mode       = LEDC_LOW_SPEED_MODE,
         .timer_num        = LEDC_TIMER_0,
         .duty_resolution  = LEDC_TIMER_12_BIT,
-        .freq_hz          = 500,  // Set output frequency at 500 Hz
+        .freq_hz          = 50,  // Set output frequency at 50 Hz
         .clk_cfg          = LEDC_AUTO_CLK
     };
     ESP_ERROR_CHECK(ledc_timer_config(&ledc_timer));
