aea8eb8d699b0bd9f883b18712df0ac7fcad25b4
Max <pinkymaxou08@gmail.com>
2023-08-22 18:56:56 -0400

    progress bar for testing connection

--- source/fwi-receiver-fw/main/oledui/UITestConn.c
+++ source/fwi-receiver-fw/main/oledui/UITestConn.c
@@ -50,14 +50,16 @@ static void DrawScreen()
     char szText[64+1];
 
     SSD1306_ClearDisplay(pss1306Handle);
-    sprintf(szText, "Checking\n%" PRIu32 " / %" PRIu32, u32ConnCount, (uint32_t)HWCONFIG_OUTPUT_COUNT);
+    sprintf(szText, "Testing. conn\n%" PRIu32 " / %" PRIu32, u32ConnCount, (uint32_t)HWCONFIG_OUTPUT_COUNT);
     SSD1306_DrawString(pss1306Handle, 15, 4, szText);
 
     const int32_t s32Width = 128 - 16*2;
-
-    const int32_t s32BarWidth = (int32_t)(MAINAPP_GetProgress()*s32Width);
-    SSD1306_FillRect(pss1306Handle, 16, 64 - 16, s32BarWidth, 16, true);
-    SSD1306_DrawRect(pss1306Handle, 16, 64 - 16, s32Width, 16, true);
-
+    const double ofOne = MAINAPP_GetProgress();
+    if (ofOne > 0.0d && ofOne < 1.0d)
+    {
+        const int32_t s32BarWidth = (int32_t)(ofOne*s32Width);
+        SSD1306_FillRect(pss1306Handle, 16, 64 - 16, s32BarWidth, 16, true);
+        SSD1306_DrawRect(pss1306Handle, 16, 64 - 16, s32Width, 16, true);
+    }
     SSD1306_UpdateDisplay(pss1306Handle);
 }
