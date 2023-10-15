e6cd1f2514cdc2416fb7c3eb1af6700fb24bfef2
Max <pinkymaxou08@gmail.com>
2023-10-15 15:15:45 -0400

    ESP-NOW Status and fire commands

--- source/fwi-receiver-fw/main/remote/ESPNow.h
+++ source/fwi-receiver-fw/main/remote/ESPNow.h
@@ -1,6 +1,32 @@
 #ifndef _ESPNOW_H_
 #define _ESPNOW_H_
 
+#include <stdbool.h>
+#include "fwi-output.h"
+#include "fwi-remote-comm.h"
+
+typedef struct
+{
+    FWIREMOTECOMM_EFRAMEID eFrameID;
+    union
+    {
+        FWIREMOTECOMM_C2SGetStatus c2sGetStatus;
+        FWIREMOTECOMM_C2SFire c2sFire;
+    } URx;
+} __attribute__((aligned)) ESPNOW_SRxFrame;
+
+typedef struct
+{
+    FWIREMOTECOMM_EFRAMEID eFrameID;
+    union
+    {
+        FWIREMOTECOMM_S2CGetStatusResp s2cGetStatusResp;
+        FWIREMOTECOMM_S2CFireResp s2cFireResp;
+    } URx;
+} __attribute__((aligned)) ESPNOW_STxFrame;
+
 void ESPNOW_Init();
+
+void ESPNOW_RunTick();
 
 #endif
